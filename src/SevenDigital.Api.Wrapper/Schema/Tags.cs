6c4f89265a0e14cd08886c038a4cc90b3cd0d00b
Greg Sochanik <greg.sochanik@7digital.com>
2011-03-10 14:51:09 +0000

    Added /tags endpoint with paging and tests

---
+++ src/SevenDigital.Api.Wrapper/Schema/Tags.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using SevenDigital.Api.Wrapper.Schema.Attributes;
+
+namespace SevenDigital.Api.Wrapper.Schema {
+
+	[Serializable]
+	[ApiEndpoint("tag")]
+	[XmlRoot("tags")]
+	public class Tags : HasPaging {
+		[XmlElement("tag")]
+		public List<Tag> TagList { get; set; }
+	}
+
+	[Serializable]
+	[XmlRoot("tag")]
+	public class Tag
+	{
+		[XmlAttribute("id")]
+		public string Id { get; set; }
+
+		[XmlElement("text")]
+		public string Text { get; set; }
+
+		[XmlElement("url")]
+		public string Url { get; set; }
+
+		[XmlElement("count")]
+		public int Count { get; set; }
+	}
+}
