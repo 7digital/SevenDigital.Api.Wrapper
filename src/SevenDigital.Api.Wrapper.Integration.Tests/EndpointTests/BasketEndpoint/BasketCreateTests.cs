6bf6a365d2a13eb550f22a6f5419d15f38ebcfc4
Greg Sochanik <greg@sochanik.com>
2011-03-12 09:24:09 +0000

    Added Basket endpoint

---
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/BasketEndpoint/BasketCreateTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SevenDigital.Api.Wrapper.Schema.Basket;
+
+namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests.BasketEndpoint
+{
+	[TestFixture]
+	public class BasketCreateTests
+	{
+		private const string EXPECTED_RELEASE_ID = "160553";
+		private string _basketId;
+		private const string EXPECTED_TRACK_ID = "1693930";
+
+		[TestFixtureSetUp]
+		public void Can_create_basket()
+		{
+			Basket basketCreate = new FluentApi<Basket>()
+				.WithEndpoint("basket/create")
+				.WithParameter("country", "GB")
+				.Resolve();
+			Assert.That(basketCreate, Is.Not.Null);
+			Assert.That(basketCreate.Id, Is.Not.Empty);
+			_basketId = basketCreate.Id;
+		}
+
+		[Test]
+		public void Can_retrieve_that_basket()
+		{
+			Basket basket = new FluentApi<Basket>()
+				.WithParameter("basketId", _basketId)
+				.Resolve();
+			Assert.That(basket, Is.Not.Null);
+			Assert.That(basket.Id, Is.EqualTo(_basketId));
+		}
+
+		[Test]
+		public void Can_add_release_to_that_that_basket()
+		{
+			Basket basket = new FluentApi<Basket>()
+				.WithEndpoint("basket/additem")
+				.WithParameter("basketId", _basketId)
+				.WithParameter("releaseId", EXPECTED_RELEASE_ID)
+				.Resolve();
+
+			Assert.That(basket, Is.Not.Null);
+			Assert.That(basket.Id, Is.EqualTo(_basketId));
+			Assert.That(basket.BasketItems.Items.Count, Is.GreaterThan(0));
+			Assert.That(basket.BasketItems.Items.FirstOrDefault().ReleaseId, Is.EqualTo(EXPECTED_RELEASE_ID));
+		}
+
+		[Test]
+		public void Can_add_track_to_that_that_basket()
+		{
+			Basket basket = new FluentApi<Basket>()
+				.WithEndpoint("basket/additem")
+				.WithParameter("basketId", _basketId)
+				.WithParameter("releaseId", EXPECTED_RELEASE_ID)
+				.WithParameter("trackId", EXPECTED_TRACK_ID)
+				.Resolve();
+
+			Assert.That(basket, Is.Not.Null);
+			Assert.That(basket.Id, Is.EqualTo(_basketId));
+			Assert.That(basket.BasketItems.Items.Count, Is.GreaterThan(0));
+			Assert.That(basket.BasketItems.Items.FirstOrDefault().TrackId, Is.EqualTo(EXPECTED_TRACK_ID));
+		}
+
+		[Test, Ignore("Not implemented yet")]
+		public void Can_remove_release_from_that_that_basket()
+		{
+
+		}
+
+		[Test, Ignore("Not implemented yet")]
+		public void Can_remove_track_from_that_that_basket()
+		{
+
+		}
+	}
+}
---
+++ src/SevenDigital.Api.Wrapper/Schema/Basket/Basket.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using SevenDigital.Api.Wrapper.Schema.Attributes;
+using SevenDigital.Api.Wrapper.Schema.Pricing;
+
+namespace SevenDigital.Api.Wrapper.Schema.Basket
+{
+	[Serializable]
+	[ApiEndpoint("basket")]
+	[XmlRoot("basket")]
+	public class Basket
+	{
+		[XmlAttribute("id")]
+		public string Id { get; set; }
+
+		[XmlElement("price")]
+		public Price Price { get; set; }
+
+		[XmlElement("basketItems")]
+		public BasketItemList BasketItems { get; set; }
+	}
+
+	[Serializable]
+	[XmlRoot("basketItems")]
+	public class BasketItemList
+	{
+		[XmlElement("basketItem")]
+		public List<BasketItem> Items { get; set; }
+	}
+
+	[Serializable]
+	[XmlRoot("basketItem")]
+	public class BasketItem
+	{
+		[XmlAttribute("id")]
+		public int Id { get; set; }
+
+		[XmlElement("type")]
+		public ItemType Type { get; set; }
+
+		[XmlElement("itemName")]
+		public string ItemName { get; set; }
+
+		[XmlElement("artistName")]
+		public string ArtistName { get; set; }
+
+		[XmlElement("trackId")]
+		public string TrackId { get; set; }
+
+		[XmlElement("releaseId")]
+		public string ReleaseId { get; set; }
+
+		[XmlElement("price")]
+		public Price Price { get; set; }
+	}
+}
