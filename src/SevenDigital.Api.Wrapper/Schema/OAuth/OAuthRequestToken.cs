5e8a9ed30a7938a73ca496341279206e8ed7758c
Greg Sochanik <greg@sochanik.com>
2011-04-04 08:01:01 +0100

    Forgot file

---
+++ src/SevenDigital.Api.Wrapper/Schema/OAuth/OAuthRequestToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml.Serialization;
+using SevenDigital.Api.Wrapper.EndpointResolution.OAuth;
+using SevenDigital.Api.Wrapper.Schema.Attributes;
+
+namespace SevenDigital.Api.Wrapper.Schema.OAuth
+{
+	[Serializable]
+	[ApiEndpoint("oauth/requesttoken")]
+	[XmlRoot("oauth_request_token")]
+	[OAuthSigned]
+	public class OAuthRequestToken
+	{
+		[XmlElement("oauth_token")]
+		public string Token { get; set; }
+
+		[XmlElement("oauth_token_secret")]
+		public string Secret { get; set; }
+	}
+}
