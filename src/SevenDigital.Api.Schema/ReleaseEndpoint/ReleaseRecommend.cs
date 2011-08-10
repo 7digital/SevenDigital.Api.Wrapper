ff7f54480f459644e61985fd6b6a63f3c1d3c77d
Phil <phil.murphy@7digital.com>
2011-08-10 16:33:32 +0100

    * added extension method for ReleaseEndpoint/ReleaseRecommend.cs

--- src/SevenDigital.Api.Schema/ReleaseEndpoint/ReleaseRecommend.cs
+++ src/SevenDigital.Api.Schema/ReleaseEndpoint/ReleaseRecommend.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using SevenDigital.Api.Schema.Attributes;
+using SevenDigital.Api.Schema.ParameterDefinitions.Get;
 
 namespace SevenDigital.Api.Schema.ReleaseEndpoint
 {
 	
 	[ApiEndpoint("release/recommend")]
 	[XmlRoot("recommendations")]
-	public class ReleaseRecommend : HasPaging
+	public class ReleaseRecommend : HasPaging, HasReleaseIdParameter
 	{
 		[XmlElement("recommendedItem")]
 		public List<RecommendedItem> RecommendedItems { get; set; }
