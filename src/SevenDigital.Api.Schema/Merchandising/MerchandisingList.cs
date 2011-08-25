c6985ab23728c402faea02d462646177f39cfb22
Antony Denyer <antonydenyer@hotmail.com>
2011-08-25 14:35:32 +0100

    fixed node name

--- src/SevenDigital.Api.Schema/Merchandising/MerchandisingList.cs
+++ src/SevenDigital.Api.Schema/Merchandising/MerchandisingList.cs
@@ -6,14 +6,14 @@ using SevenDigital.Api.Schema.ParameterDefinitions.Get;
 namespace SevenDigital.Api.Schema.Merchandising
 {
 	[ApiEndpoint("merchandising/list/details")]
-	[XmlRoot("List")]
+	[XmlRoot("list")]
 	public class MerchandisingList : HasKeyParameter
 	{
 		[XmlElement("key")]
 		public string Key { get; set; }
 
-		[XmlArray("Items")]
-		[XmlArrayItem("Item")]
+		[XmlArray("listItems")]
+		[XmlArrayItem("listItem")]
 		public List<ListItem> Items { get; set; }
 	}
 }
