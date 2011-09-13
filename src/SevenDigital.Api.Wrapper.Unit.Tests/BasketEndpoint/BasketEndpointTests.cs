ce913035336125c7554dbc67ea98e5a3cb40522c
michael <michael.lam@7digital.com>
2011-09-13 10:53:01 +0100

    added test to assert fluentapi trackId parameter is removed upon adding a release to basket

---
+++ src/SevenDigital.Api.Wrapper.Unit.Tests/BasketEndpoint/BasketEndpointTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using SevenDigital.Api.Schema.Basket;
+using SevenDigital.Api.Wrapper.Extensions.Post;
+
+namespace SevenDigital.Api.Wrapper.Unit.Tests.BasketEndpoint
+{
+	[TestFixture]
+	public class BasketEndpointTests
+	{
+		[Test]
+		public void Should_remove_track_id_parameter_when_adding_a_release_to_basket()
+		{
+			var basketEndpoint = new FluentApi<Basket>();
+			basketEndpoint.AddItem(Guid.NewGuid(),1,1);
+			Assert.That(basketEndpoint.Parameters.Keys.Contains("trackId"));
+			basketEndpoint.AddItem(Guid.NewGuid(),1);
+			Assert.That(basketEndpoint.Parameters.Keys.Contains("trackId"),Is.False);
+		}
+	}
+}
