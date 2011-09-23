a0b3abe9550143bd9d46681d6a8bacee16a0d8bf
Antony Denyer <antonydenyer@hotmail.com>
2011-09-23 17:05:30 +0100

    rename purchase id parameter

--- src/SevenDigital.Api.Wrapper/Extensions/HasPurchaseIdParameterExtensions.cs
+++ src/SevenDigital.Api.Wrapper/Extensions/HasPurchaseIdParameterExtensions.cs
@@ -2,9 +2,9 @@ using SevenDigital.Api.Schema.ParameterDefinitions.Get;
 
 namespace SevenDigital.Api.Wrapper
 {
-	public static class HasPurchaseIdParameter
+	public static class HasPurchaseIdParameterExtensions
 	{
-		public static IFluentApi<T> WithPurchaseId<T>(this IFluentApi<T> api, string basketId) where T : HasBasketParameter
+		public static IFluentApi<T> WithPurchaseId<T>(this IFluentApi<T> api, string basketId) where T : HasPurchaseIdParameter
 		{
 			api.WithParameter("basketId", basketId);
 			return api;
