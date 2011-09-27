a6fdf486ae04cb69e44730f6d7f289ce4cd04b3a
Greg Sochanik <greg.sochanik@7digital.com>
2011-09-27 12:03:29 +0100

    Wrote more tests around XmlErrorHandler

--- src/SevenDigital.Api.Wrapper.Unit.Tests/Utility/Serialization/XmlErrorHandlerTests.cs
+++ src/SevenDigital.Api.Wrapper.Unit.Tests/Utility/Serialization/XmlErrorHandlerTests.cs
@@ -1,5 +1,6 @@
 using System.Xml.Linq;
 using NUnit.Framework;
+using SevenDigital.Api.Wrapper.Exceptions;
 using SevenDigital.Api.Wrapper.Utility.Serialization;
 
 namespace SevenDigital.Api.Wrapper.Unit.Tests.Utility.Serialization
@@ -8,7 +9,7 @@ namespace SevenDigital.Api.Wrapper.Unit.Tests.Utility.Serialization
 	public class XmlErrorHandlerTests
 	{
 		[Test]
-		public void Should_get_response_as_xml_with_valid_xml() {
+		public void Should_get_response_as_xml_with_valid_api_xml() {
 			const string validXml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><response status=\"ok\" version=\"1.2\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://api.7digital.com/1.2/static/7digitalAPI.xsd\" ><serviceStatus><serverTime>2011-09-27T10:45:06Z</serverTime></serviceStatus></response>";
 			var xmlErrorHandler = new XmlErrorHandler();
 
@@ -34,6 +35,30 @@ namespace SevenDigital.Api.Wrapper.Unit.Tests.Utility.Serialization
 			XElement responseAsXml = xmlErrorHandler.GetResponseAsXml(validXml);
 
 			Assert.That(responseAsXml.Name.LocalName, Is.EqualTo("response"));
+		}
+
+		[Test]
+		public void Should_assert_error_with_valid_api_xml() {
+			const string validXml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><response status=\"error\" version=\"1.2\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://api.7digital.com/1.2/static/7digitalAPI.xsd\" ><error code=\"1001\"><errorMessage>Requires parameter q is missing</errorMessage></error></response>";
+			var xmlErrorHandler = new XmlErrorHandler();
+			XElement responseAsXml = xmlErrorHandler.GetResponseAsXml(validXml);
+			Assert.Throws<ApiXmlException>(() => xmlErrorHandler.AssertError(responseAsXml));
+		}
+
+		[Test]
+		public void Should_not_fail_if_xml_is_a_malformed_api_error() {
+			const string validXml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><response status=\"error\" version=\"1.2\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://api.7digital.com/1.2/static/7digitalAPI.xsd\" ><error><errorme></errorme></error></response>";
+			var xmlErrorHandler = new XmlErrorHandler();
+			XElement responseAsXml = xmlErrorHandler.GetResponseAsXml(validXml);
+			Assert.Throws<ApiXmlException>(() => xmlErrorHandler.AssertError(responseAsXml));
+		}
+
+		[Test]
+		public void Should_not_fail_if_xml_is_not_an_api_error() {
+			const string validXml = "<xml>Hello world</xml>";
+			var xmlErrorHandler = new XmlErrorHandler();
+			XElement responseAsXml = xmlErrorHandler.GetResponseAsXml(validXml);
+			Assert.DoesNotThrow(() => xmlErrorHandler.AssertError(responseAsXml));
 		}
 	}
 }
