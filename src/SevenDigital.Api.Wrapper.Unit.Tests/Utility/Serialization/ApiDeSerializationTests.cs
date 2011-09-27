df390de9c1e4f2732ce64d1ba04326c3f94062f8
Greg Sochanik <greg.sochanik@7digital.com>
2011-09-27 11:38:51 +0100

    Moved XmlErrorHandler logic out to enable unit testing

--- src/SevenDigital.Api.Wrapper.Unit.Tests/Utility/Serialization/ApiDeSerializationTests.cs
+++ src/SevenDigital.Api.Wrapper.Unit.Tests/Utility/Serialization/ApiDeSerializationTests.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using FakeItEasy;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using SevenDigital.Api.Schema;
-using SevenDigital.Api.Wrapper.EndpointResolution;
 using SevenDigital.Api.Wrapper.Exceptions;
 using SevenDigital.Api.Wrapper.Utility.Serialization;
 
@@ -17,7 +11,7 @@ namespace SevenDigital.Api.Wrapper.Unit.Tests.Utility.Serialization
         [Test]
         public void Should_throw_api_exception_with_correct_error_if_error_xml_received()
         {
-            var apiXmlDeSerializer = new ApiXmlDeSerializer<Status>(null);
+            var apiXmlDeSerializer = new ApiXmlDeSerializer<Status>(null, new XmlErrorHandler());
 
             var apiException = Assert.Throws<ApiXmlException>(() => apiXmlDeSerializer.DeSerialize(string.Empty));
             Assert.That(apiException.Message, Is.EqualTo("An error has occured in the Api, see Error property for details"));
