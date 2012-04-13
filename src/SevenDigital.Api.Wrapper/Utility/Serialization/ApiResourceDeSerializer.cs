68ba6d846e301ace6686e801fa0596378b48d0b3
Raoul Millais <raoul@raoulmillais.com>
2012-04-13 15:52:38 +0100

    Guard for InvalidOperationException when deserializing errors and wrap in ApiXmlException to bubble up

--- src/SevenDigital.Api.Wrapper/Utility/Serialization/ApiResourceDeSerializer.cs
+++ src/SevenDigital.Api.Wrapper/Utility/Serialization/ApiResourceDeSerializer.cs
@@ -1,19 +1,28 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Xml.Serialization;
+using SevenDigital.Api.Wrapper.Exceptions;
 
 namespace SevenDigital.Api.Wrapper.Utility.Serialization
 {
-    public class ApiResourceDeSerializer<T> : IDeSerializer<T> where T : class
-    {
-        public T DeSerialize(string response)
-        {
-            var ser = new XmlSerializer(typeof(T));
-            using (var reader = new StringReader(response))
-            {
-                object obj = ser.Deserialize(reader);
-                var instance = (T)obj;
-                return instance;
-            }
-        }
-    }
+	public class ApiResourceDeSerializer<T> : IDeSerializer<T> where T : class
+	{
+		public T DeSerialize(string response)
+		{
+			var ser = new XmlSerializer(typeof(T));
+			using (var reader = new StringReader(response))
+			{
+				try
+				{
+					object obj = ser.Deserialize(reader);
+					var instance = (T)obj;
+					return instance;
+				}
+				catch(InvalidOperationException ioex)
+				{
+					throw new ApiXmlException("Error trying to deserialize error response xml", response, ioex);
+				}
+			}
+		}
+	}
 }
