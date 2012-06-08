6d5d1a81d404010870689f421947d47c97c7a6f3
Antony Denyer <antonydenyer@gmail.com>
2012-06-08 10:51:38 +0100

    added has tags parameter to artists by tag

--- src/SevenDigital.Api.Schema/Tags/ArtistByTagTop.cs
+++ src/SevenDigital.Api.Schema/Tags/ArtistByTagTop.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Xml.Serialization;
 using SevenDigital.Api.Schema.Attributes;
+using SevenDigital.Api.Schema.ParameterDefinitions.Get;
 
 namespace SevenDigital.Api.Schema.Tags
 {
 	[ApiEndpoint("artist/bytag/top")]
 	[XmlRoot("taggedResults")]
-	public class ArtistByTagTop : HasPaging
+	public class ArtistByTagTop : HasPaging, HasTags
 	{
 		[XmlElement("type")]
 		public ItemType Type { get; set; }
