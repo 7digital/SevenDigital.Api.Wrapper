08e1904a7e271e01eb6d7cd44ccbd0eea19eb67c
Anthony Steele <anthony.steele@7digital.com>
2012-06-12 17:13:33 +0100

    fix deserialisation in the case where the status is Ok but the empty response object is missing

---
+++ src/SevenDigital.Api.Wrapper.Unit.Tests/Deserialisation/User/Payment/DeleteCardTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NUnit.Framework;
+using SevenDigital.Api.Schema.Payment;
+using SevenDigital.Api.Schema.User.Payment;
+using SevenDigital.Api.Wrapper.Utility.Http;
+using SevenDigital.Api.Wrapper.Utility.Serialization;
+
+namespace SevenDigital.Api.Wrapper.Unit.Tests.Deserialisation.User.Payment
+{
+	[TestFixture]
+	public class DeleteCardTests
+	{
+		[Test]
+		public void Can_Deserialise_ok_response_without_body__as_DeleteCard()
+		{
+			const string ResponseXml = "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?><response status=\"ok\" version=\"1.2\" />";
+
+			var response = new Response
+			    {
+			        StatusCode = HttpStatusCode.OK,
+			        Body = ResponseXml
+			    };
+
+			var xmlSerializer = new ResponseDeserializer<DeleteCard>();
+			var result = xmlSerializer.Deserialize(response);
+
+			Assert.That(result, Is.Not.Null);
+		}
+	}
+}
