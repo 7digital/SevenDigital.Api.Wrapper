e562efb8180e4a31140699c4f818e9b5a6958b78
Anthony Steele <anthony.steele@7digital.com>
2012-08-08 11:02:03 +0100

    merged georestriction changes

--- src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/TerritoriesEndpoint/TerritoriesEndpointTests.cs
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/TerritoriesEndpoint/TerritoriesEndpointTests.cs
@@ -1,24 +1,60 @@
-﻿using System.Linq;
-using NUnit.Framework;
-using SevenDigital.Api.Schema.Merchandising;
+﻿using NUnit.Framework;
 using SevenDigital.Api.Schema.Territories;
 
 namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests.TerritoriesEndpoint
 {
-	[TestFixture]
+	[TestFixture, Ignore("Permissions not set")]
 	public class TerritoriesEndpointTests
 	{
-		[Test, Ignore("In beta testing")]
-		public void Can_hit_fluent_endpoint_for_territories()
+		[Test]
+		public void Can_hit_fluent_endpoint_for_country_lookup()
 		{
-			var merchList = Api<GeoIpLookup>
+			var countryForIp = Api<GeoIpLookup>
 				.Create
 				.WithIpAddress("84.45.95.241")
 				.WithParameter("shopId", "34")
 				.Please();
 
-			Assert.That(merchList, Is.Not.Null);
-			Assert.That(merchList.CountryCode, Is.EqualTo("7"));
+			Assert.That(countryForIp, Is.Not.Null);
+			Assert.That(countryForIp.CountryCode, Is.EqualTo("7"));
+		}
+
+		[Test]
+		public void Can_hit_fluent_endpoint_for_checkout_restrictions_with_checkout_allowed()
+		{
+			var restrictions = Api<GeoRestrictions>
+				.Create
+				.WithIpAddress("84.45.95.241")
+				.WithParameter("shopId", "34")
+				.Please();
+
+			Assert.That(restrictions, Is.Not.Null);
+			Assert.That(restrictions.AllowCheckout, Is.True);
+		}
+
+		[Test]
+		public void Can_hit_fluent_endpoint_for_checkout_restrictions_with_checkout_not_allowed()
+		{
+			var restrictions = Api<GeoRestrictions>
+				.Create
+				.WithIpAddress("1.2.3.4")
+				.WithParameter("shopId", "34")
+				.Please();
+
+			Assert.That(restrictions, Is.Not.Null);
+			Assert.That(restrictions.AllowCheckout, Is.False);
+		}
+
+		[Test]
+		public void Can_hit_fluent_endpoint_for_territories_checkout_restrictions_with_ip_only()
+		{
+			var restrictions = Api<GeoRestrictions>
+				.Create
+				.WithIpAddress("84.45.95.241")
+				.Please();
+
+			Assert.That(restrictions, Is.Not.Null);
+			Assert.That(restrictions.AllowCheckout, Is.True);
 		}
 	}
 }
