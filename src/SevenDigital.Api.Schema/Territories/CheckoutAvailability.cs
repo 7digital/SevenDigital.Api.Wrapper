7fbf8f7ab16ad0d754940741e914828ee5b6c2d3
Henry Oswald <henry.oswald@7digital.com>
2012-11-05 12:11:36 +0000

    updated caught exception to the new ApiException inside the new wrapper execptions

---
+++ src/SevenDigital.Api.Schema/Territories/CheckoutAvailability.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+using SevenDigital.Api.Schema.Attributes;
+using SevenDigital.Api.Schema.ParameterDefinitions.Get;
+
+namespace SevenDigital.Api.Schema.Territories
+{
+	[ApiEndpoint("country/georestrictions/checkout")]
+	[XmlRoot("GeoIpLookup")]
+	public class CheckoutAvailability : HasIpAddressParameter
+	{
+		[XmlElement("countryId")]
+		public string CountryId { get; set; }
+
+		[XmlElement("allowCheckout")]
+		public bool AllowCheckout { get; set; }
+	}
+}
