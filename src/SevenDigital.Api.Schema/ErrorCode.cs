d6cbb5b0ee3c2c060f613fbb39a95611edb5b602
Anthony Steele <anthony.steele@7digital.com>
2012-11-09 15:10:24 +0000

    rebased

---
+++ src/SevenDigital.Api.Schema/ErrorCode.cs
@@ -0,0 +1,36 @@
+﻿namespace SevenDigital.Api.Schema
+{
+	public enum ErrorCode
+	{
+		Unknown = 0,
+
+		RequiredParameterMissing = 1001,
+		InvalidParameterValue = 1002,
+		ParameterOutOfAllowableRange = 1003,
+		InvalidEnumerationValue = 1006,
+
+		ResourceNotFound = 2001,
+		ResourceNotAvailableInCurrentContext = 2002,
+		ResourceAlreadyExists = 2003,
+
+		UserCardHasExpired = 3001,
+		UserHasNoCardDetails = 3002,
+		PaymentFailed = 3003,
+		PriceSuppliedDoesNotMatch = 3004,
+		PaymentProviderConnectionError = 3100,
+		PaymentFailedError = 3101,
+		PaymentDeclinedError = 3102,
+		AddCardFailedError = 3103,
+		AddCardDeclinedError = 3104,
+		RefundBasketNotAllowedError = 3105,
+		RefundDeclinedError = 3106,
+		TransactionAlreadyRefundedError = 3017,
+
+		UnableToPerformAction = 7001,
+		ApplicationConfigurationError = 7002,
+		OperationTimedOut = 7003,
+		RefundFailedError = 7102,
+
+		UnexpectedInternalServerError = 9001
+	}
+}
--- src/SevenDigital.Api.Wrapper/Exceptions/UserCardException.cs
+++ src/SevenDigital.Api.Wrapper/Exceptions/UserCardException.cs
@@ -13,8 +13,13 @@ namespace SevenDigital.Api.Wrapper.Exceptions
 		{
 		}
 
-		public UserCardException(string message, Response response, Error error)
-			: base(message, response, error)
+		public UserCardException(string message, ErrorCode errorCode)
+			: base(message, errorCode)
+		{
+		}
+
+		public UserCardException(string message, Response response, ErrorCode errorCode)
+			: base(message, response, errorCode)
 		{
 		}
 
