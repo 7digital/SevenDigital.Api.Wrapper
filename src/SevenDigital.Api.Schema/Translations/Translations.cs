e77f01a25f1ce0618d3e0d415ab9a851405c43a0
Dan Haller <dan.haller@7digital.com>
2012-11-12 11:30:09 +0000

    Added translations endpoint

---
+++ src/SevenDigital.Api.Schema/Translations/Translations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using SevenDigital.Api.Schema.Attributes;
+
+namespace SevenDigital.Api.Schema.Translations
+{
+	[ApiEndpoint("translations")]
+	[DataContract(Name="translations")]
+	[Serializable]
+	public class Translations : HasPaging
+	{
+		[XmlElement("translation")]
+		[DataMember(Name="translation")]
+		public List<Translation> TranslationItems { get; set; }
+	}
+
+	public class Translation
+	{
+		[XmlElement("key")]
+		public string Key { get; set; }
+
+		[XmlElement("translatedText")]
+		public string TranslatedText { get; set; }
+	}
+}
