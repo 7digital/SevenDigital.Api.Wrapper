2f0e94f815a2786290f6f1b39ba5053768419039
greg Sochanik <greg@sochanik.com>
2012-11-15 15:55:55 +0000

    Move attribute validation logic out of FluentApi and into separate class. This also moves the instantiation of the RequestData object into AttributeValidation as its now this that provides the initial requestdata setup. Keeping it simple at the moment, have started to separate the attribute code into private classes and the AttributeValidtion class is instatiated with the FluentApi ctor for the time being, but makes the ctor much cleaner and could be added to in the future

---
+++ src/SevenDigital.Api.Wrapper.Unit.Tests/AttributeManagement/AttributeValidationTest.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using SevenDigital.Api.Schema.Attributes;
+using SevenDigital.Api.Schema.OAuth;
+using SevenDigital.Api.Wrapper.AttributeManagement;
+
+namespace SevenDigital.Api.Wrapper.Unit.Tests.AttributeManagement
+{
+	[TestFixture]
+	public class AttributeValidationTests
+	{
+		[Test]
+		public void Sets_correct_uri_based_on_apiEndpoint()
+		{
+			var attributeValidation = new AttributeValidation<StubEndpoint>();
+			var requestData = attributeValidation.Validate();
+
+			Assert.That(requestData.UriPath, Is.EqualTo("me/endpoint"));
+		}
+
+		[Test]
+		public void Sets_IsSigned_if_OAuthSigned_specified()
+		{
+			var attributeValidation = new AttributeValidation<StubSecureEndpoint>();
+			var requestData = attributeValidation.Validate();
+
+			Assert.That(requestData.IsSigned);
+		}
+
+		[Test]
+		public void Sets_IsSigned_false_if_OAuthSigned_not_specified()
+		{
+			var attributeValidation = new AttributeValidation<StubEndpoint>();
+			var requestData = attributeValidation.Validate();
+
+			Assert.That(requestData.IsSigned, Is.False);
+		}
+
+		[Test]
+		public void Sets_UseHttps_if_RequireSecure_specified()
+		{
+			var attributeValidation = new AttributeValidation<StubSecureEndpoint>();
+			var requestData = attributeValidation.Validate();
+
+			Assert.That(requestData.UseHttps);
+		}
+
+		[Test]
+		public void Sets_UseHttps_false_if_RequireSecure_not_specified()
+		{
+			var attributeValidation = new AttributeValidation<StubEndpoint>();
+			var requestData = attributeValidation.Validate();
+
+			Assert.That(requestData.UseHttps, Is.False);
+		}
+
+		[Test]
+		public void Sets_Httpethod_to_POST_if_HttpPost_specified()
+		{
+			var attributeValidation = new AttributeValidation<StubPostEndpoint>();
+			var requestData = attributeValidation.Validate();
+
+			Assert.That(requestData.HttpMethod, Is.EqualTo("POST"));
+		}
+
+		[Test]
+		public void Httpethod_tdefaults_to_GET_if_HttpPost_not_specified()
+		{
+			var attributeValidation = new AttributeValidation<StubEndpoint>();
+			var requestData = attributeValidation.Validate();
+
+			Assert.That(requestData.HttpMethod, Is.EqualTo("GET"));
+		}
+	}
+
+
+	[ApiEndpoint("me/secure/endpoint")]
+	[OAuthSigned]
+	[RequireSecure]
+	public class StubSecureEndpoint
+	{}
+
+	[ApiEndpoint("me/endpoint")]
+	public class StubEndpoint
+	{}
+
+	[ApiEndpoint("me/post/endpoint")]
+	[HttpPost]
+	public class StubPostEndpoint
+	{}
+}
