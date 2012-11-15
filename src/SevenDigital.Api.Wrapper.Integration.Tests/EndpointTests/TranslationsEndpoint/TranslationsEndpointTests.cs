bd0c72513431d8d999b0aade1092395d08d9d14c
Dan Haller <dan.haller@7digital.com>
2012-11-15 10:25:44 +0000

    Ignored translation endpoint tests because the api is not public yet
    - Confirmed tests passed with oauth key that can see internal apis
    - Ignored the tests
    - Fixed up some incorrect Xml serialisation attributes

--- src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/TranslationsEndpoint/TranslationsEndpointTests.cs
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/TranslationsEndpoint/TranslationsEndpointTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using SevenDigital.Api.Schema.Translations;
 
 namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests.TranslationsEndpoint
@@ -6,34 +7,32 @@ namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests.TranslationsE
 	[TestFixture]
 	public class TranslationsEndpointTests
 	{
-		[Test]
+		[Test, Ignore("Not yet public")]
 		public void Should_hit_translations_endpoint()
 		{
 			var translations = Api<Translations>
 				.Create
-				.WithParameter("shopId", "34")
-				.WithParameter("defaultLanguageId", "1")
 				.Please();
 
 			Assert.That(translations, Is.Not.Null);
 			Assert.That(translations.TotalItems, Is.GreaterThan(0));
+			Assert.That(translations.TranslationItems.FirstOrDefault(), Is.Not.Null);
 		}
 
-		[Test]
+		[Test, Ignore("Not yet public")]
 		public void Should_hit_translations_endpoint_with_paging()
 		{
 			var translations = Api<Translations>
 				.Create
-				.WithParameter("shopId", "34")
-				.WithParameter("defaultLanguageId", "1")
 				.WithPageSize(1)
-				.WithPageNumber(1)
+				.WithPageNumber(2)
 				.Please();
 
 			Assert.That(translations, Is.Not.Null);
 			Assert.That(translations.TotalItems, Is.GreaterThan(0));
+			Assert.That(translations.TranslationItems.FirstOrDefault(), Is.Not.Null);
 			Assert.That(translations.Page, Is.EqualTo(1));
-			Assert.That(translations.PageSize, Is.EqualTo(1));
+			Assert.That(translations.PageSize, Is.EqualTo(2));
 		}
 	}
 }
