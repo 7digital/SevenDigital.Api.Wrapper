946554102c986a91eca0568e104f1cb6882fade6
Anthony Steele <anthony.steele@7digital.com>
2012-11-19 14:05:19 +0000

    Used the HasAffiliatePartnerParameter marker interface to lock down the ForAffiliatePartner extension

--- src/SevenDigital.Api.Wrapper/Extensions/AffiliatePartnerExtensions.cs
+++ src/SevenDigital.Api.Wrapper/Extensions/AffiliatePartnerExtensions.cs
@@ -1,8 +1,10 @@
-﻿namespace SevenDigital.Api.Wrapper.Extensions
+﻿using SevenDigital.Api.Schema.ParameterDefinitions.Get;
+
+namespace SevenDigital.Api.Wrapper.Extensions
 {
 	public static class AffiliatePartnerExtensions
 	{
-		public static IFluentApi<T> ForAffiliatePartner<T>(this IFluentApi<T> api, int partnerId)
+		public static IFluentApi<T> ForAffiliatePartner<T>(this IFluentApi<T> api, int partnerId) where T : HasAffiliatePartnerParameter
 		{
 			api.WithParameter("affiliatePartner", partnerId.ToString());
 			return api;
