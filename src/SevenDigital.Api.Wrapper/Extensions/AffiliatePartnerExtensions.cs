91b51ef152df766783b85bbefcf2f78e13c4a723
Anthony Steele <anthony.steele@7digital.com>
2012-11-19 12:17:46 +0000

    Push the Affiliate partner parameter setting into the wrapper to make client code more readable.

---
+++ src/SevenDigital.Api.Wrapper/Extensions/AffiliatePartnerExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace SevenDigital.Api.Wrapper.Extensions
+{
+	public static class AffiliatePartnerExtensions
+	{
+		public static IFluentApi<T> ForAffiliatePartner<T>(this IFluentApi<T> api, int partnerId)
+		{
+			api.WithParameter("affiliatePartner", partnerId.ToString());
+			return api;
+		}
+	}
+}
