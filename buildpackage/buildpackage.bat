ab2e93aacf61f98b16dcec58e112c68b49e84234
Anthony Steele <Anthony.Steele@7digital.com>
2013-02-18 16:35:32 +0000

    Updated nuget package build files

--- buildpackage/buildpackage.bat
+++ buildpackage/buildpackage.bat
@@ -6,8 +6,8 @@ nuget pack SevenDigital.Api.Wrapper.nuspec
 
 rem remember to set the 7digital api key first
 rem uncomment the next line to push to http://nuget.org
-rem remember to set the version number - or automate incrementing this?
-rem NuGet Push SevenDigital.Api.Wrapper.2.0.2.nupkg
+rem remember to set the version number here and the the .nupkg - or automate incrementing this?
+rem NuGet Push SevenDigital.Api.Wrapper.2.0.3.nupkg
 
 echo "done"
 
