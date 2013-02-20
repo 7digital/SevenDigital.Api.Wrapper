8058eeebd3cfd4fd306f22cd6e90543dc3ef8ad7
Dan Haller <dan.haller@7digital.com>
2013-02-20 09:39:34 +0000

    Add support for new release/editorial endpoint (not yet public)

---
+++ src/SevenDigital.Api.Schema/ReleaseEndpoint/ReleaseEditorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Serialization;
+using SevenDigital.Api.Schema.Attributes;
+using SevenDigital.Api.Schema.ParameterDefinitions.Get;
+
+namespace SevenDigital.Api.Schema.ReleaseEndpoint
+{
+	[XmlRoot("editorial")]
+	[ApiEndpoint("release/editorial")]
+	[Serializable]
+	public class ReleaseEditorial : HasReleaseIdParameter
+	{
+		[XmlElement("review")]
+		public Review Review { get; set; }
+
+		[XmlElement("staffRecommendation")]
+		public Review StaffRecommendation { get; set; }
+
+		[XmlElement("promotionalText")]
+		public string PromotionalText { get; set; }
+	}
+
+	public class Review
+	{
+		[XmlElement("text")]
+		public string Text { get; set; }
+	}
+}
