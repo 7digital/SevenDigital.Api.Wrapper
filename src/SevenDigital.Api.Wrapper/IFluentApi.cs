2d4fa9edec9ff2b513a2e8c4b02d9eb03618dc6e
Anthony Steele <Anthony.Steele@7digital.com>
2013-05-10 16:35:43 +0100

    UsingCache needs to be on the IFluentApi interface

--- src/SevenDigital.Api.Wrapper/IFluentApi.cs
+++ src/SevenDigital.Api.Wrapper/IFluentApi.cs
@@ -12,6 +12,7 @@ namespace SevenDigital.Api.Wrapper
 		IFluentApi<T> ClearParameters();
 		IFluentApi<T> ForUser(string token, string secret);
 		IFluentApi<T> UsingClient(IHttpClient httpClient);
+		IFluentApi<T> UsingCache(IResponseCache responseCache);
 
 		T Please();
 		void PleaseAsync(Action<T> callback);
