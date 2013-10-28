13cab2683c33b0248925ab46c3e4fc2a01760e1b
Dave Aitken <dave.aitken@gmail.com>
2013-10-28 10:58:22 +0000

    Add cardregistration endpoints to schema for transparent post

---
+++ src/SevenDigital.Api.Schema/User/Payment/CardRegistrationStatus.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Xml.Serialization;
+
+namespace SevenDigital.Api.Schema.User.Payment
+{
+	public enum CardRegistrationStatus
+	{
+		[XmlEnum(Name = "pending")]
+		Pending,
+		[XmlEnum(Name = "complete")]
+		Complete,
+		[XmlEnum(Name = "error")]
+		Error,
+	}
+}
