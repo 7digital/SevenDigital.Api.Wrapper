9785a221bd5e07e4d18416b2008ca9178ceeedde
Matt Gray <matt.gray@7digital.com>
2014-01-07 15:26:16 +0000

    Remove unused ToQueryStringNoUrlEncode method

--- src/SevenDigital.Api.Wrapper.Unit.Tests/Http/DictionaryExtensionsTests.cs
+++ src/SevenDigital.Api.Wrapper.Unit.Tests/Http/DictionaryExtensionsTests.cs
@@ -8,27 +8,11 @@ namespace SevenDigital.Api.Wrapper.Unit.Tests.Http
 	public class DictionaryExtensionsTests
 	{
 		[Test]
-		public void Should_return_correct_nvc_as_querystring()
-		{
-			var expected =  new Dictionary<string,string> { {"artistId", "1234"},{"country", "GB"}};
-			string querystring = expected.ToQueryStringNoUrlEncode();
-			Assert.That(querystring, Is.EqualTo("artistId=1234&country=GB"));
-		}
-
-		[Test]
 		public void Should_urlencode_if_specified_return_correct_nvc_as_querystring()
 		{
 			var expected = new Dictionary<string, string> { { "q", "Alive & Amplified" }, { "country", "GB" } };
 			string querystring = expected.ToQueryString();
 			Assert.That(querystring, Is.EqualTo("q=Alive%20%26%20Amplified&country=GB"));
-		}
-
-		[Test]
-		public void Should_not_urlencode_if_specified_return_correct_nvc_as_querystring()
-		{
-			var expected = new Dictionary<string, string> { { "q", "ou est tu" }, { "country", "GB" } };
-			string querystring = expected.ToQueryStringNoUrlEncode();
-			Assert.That(querystring, Is.EqualTo("q=ou est tu&country=GB"));
 		}
 	}
 }
--- src/SevenDigital.Api.Wrapper/EndpointResolution/DictionaryExtensions.cs
+++ src/SevenDigital.Api.Wrapper/EndpointResolution/DictionaryExtensions.cs
@@ -6,16 +6,6 @@ namespace SevenDigital.Api.Wrapper.EndpointResolution
 {
 	public static class DictionaryExtensions
 	{
-		public static string ToQueryStringNoUrlEncode(this IDictionary<string,string> collection)
-		{
-			var sb = new StringBuilder();
-			foreach (var key in collection.Keys)
-			{
-				sb.AppendFormat("{0}={1}&", key, collection[key]);
-			}
-			return sb.ToString().TrimEnd('&');
-		}
-
 		public static string ToQueryString(this IDictionary<string, string> collection)
 		{
 			var sb = new StringBuilder();
