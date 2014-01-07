d114a14552b282d317edfa191af45e838e1aadb2
Matt Gray <matt.gray@7digital.com>
2014-01-07 15:30:27 +0000

    Use UrlEncode from OAuth lib

--- src/SevenDigital.Api.Wrapper/EndpointResolution/DictionaryExtensions.cs
+++ src/SevenDigital.Api.Wrapper/EndpointResolution/DictionaryExtensions.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
+using OAuth;
 
 namespace SevenDigital.Api.Wrapper.EndpointResolution
 {
@@ -11,7 +11,7 @@ namespace SevenDigital.Api.Wrapper.EndpointResolution
 			var sb = new StringBuilder();
 			foreach (var key in collection.Keys)
 			{
-				var parameter = Uri.EscapeDataString(collection[key]);
+				var parameter = OAuthTools.UrlEncodeRelaxed(collection[key]);
 				sb.AppendFormat("{0}={1}&", key, parameter);
 			}
 			return sb.ToString().TrimEnd('&');
