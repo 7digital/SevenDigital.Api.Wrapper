d0d505a76dbbff699102dc42b16d28e7a344f7a4
greg Sochanik <greg@sochanik.com>
2014-02-19 15:35:01 +0000

    Renamed test files

--- src/SevenDigital.Api.Wrapper.Unit.Tests/Endpoints/Playlists/PlaylistXmlTests.cs
+++ src/SevenDigital.Api.Wrapper.Unit.Tests/Endpoints/Playlists/PlaylistXmlTests.cs
@@ -13,52 +13,6 @@ using SevenDigital.Api.Wrapper.Responses;
 namespace SevenDigital.Api.Wrapper.Unit.Tests.Endpoints.Playlists
 {
 	[TestFixture]
-	public class UserPlaylistsXmlTests
-	{
-		private UserPlaylists _userPlaylists;
-
-		[SetUp]
-		public void SetUp()
-		{
-			var requestBuilder = A.Fake<IRequestBuilder>();
-			var httpClient = A.Fake<IHttpClient>();
-			var responseXml = File.ReadAllText("StubResponses/Playlists.xml");
-			var validPlaylistsResponse = new Response(HttpStatusCode.OK, responseXml);
-			A.CallTo(() => httpClient.Send(null)).WithAnyArguments().Returns(validPlaylistsResponse);
-			var fluentApi = new FluentApi<UserPlaylists>(httpClient, requestBuilder);
-
-			_userPlaylists = fluentApi.Please();
-		}
-
-		[Test]
-		public void then_there_are_the_correct_amount_of_playlists()
-		{
-			Assert.That(_userPlaylists.TotalItems, Is.EqualTo(4));
-			Assert.That(_userPlaylists.Playlists.Count, Is.EqualTo(4));
-			Assert.That(_userPlaylists.Page, Is.EqualTo(1));
-			Assert.That(_userPlaylists.PageSize, Is.EqualTo(10));
-		}
-
-		[Test]
-		public void then_the_links_have_been_deserialized_correctly()
-		{
-			var firstOrDefault = _userPlaylists.Playlists.FirstOrDefault();
-			Assert.That(firstOrDefault.Links.Count, Is.EqualTo(3));
-		}
-
-		[Test]
-		public void then_everything_else_has_been_deserialized_correctly()
-		{
-			var firstOrDefault = _userPlaylists.Playlists.FirstOrDefault();
-			Assert.That(firstOrDefault.Id, Is.EqualTo("52b3733cc902160fa8bc9f34"));
-			Assert.That(firstOrDefault.LastUpdated.ToString("dd/MM/yyyy hh:mm:ss"), Is.EqualTo(new DateTime(2014, 1, 22, 14, 52, 21).ToString("dd/MM/yyyy hh:mm:ss"))); 
-			Assert.That(firstOrDefault.Name, Is.EqualTo("Album From Catalogue"));
-			Assert.That(firstOrDefault.TrackCount, Is.EqualTo(4));
-			Assert.That(firstOrDefault.Visibility, Is.EqualTo(PlaylistVisibilityType.Private));
-		}
-	}
-
-	[TestFixture]
 	public class PlaylistXmlTests
 	{
 		private Playlist _playlist;
