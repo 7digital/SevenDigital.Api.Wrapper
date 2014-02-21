d212ad9194f37043663ac1ddd9669794c1ad9640
greg Sochanik <greg@sochanik.com>
2014-02-21 13:27:40 +0000

    Added missing test class

--- src/SevenDigital.Api.Wrapper.Unit.Tests/Requests/Serializing/XmlTransferContentTypeTest.cs
+++ src/SevenDigital.Api.Wrapper.Unit.Tests/Requests/Serializing/XmlTransferContentTypeTest.cs
@@ -40,29 +40,4 @@ namespace SevenDigital.Api.Wrapper.Unit.Tests.Requests.Serializing
 			Assert.That(xml, Is.EqualTo(expectedXmlOutput));
 		}
 	}
-
-	[TestFixture]
-	public class JsonTransferContentTypeTest
-	{
-		private JsonTransferContentType _transferContentType;
-
-		[SetUp]
-		public void SetUp()
-		{
-			_transferContentType = new JsonTransferContentType();
-		}
-
-		[Test]
-		public void Should_have_correct_contenttype()
-		{
-			Assert.That(_transferContentType.ContentType, Is.EqualTo("application/json"));
-		}
-
-		[Test]
-		[Ignore("Not implemented yet")]
-		public void SHould_serialize_artist_as_expected()
-		{
-			
-		}
-	}
 }
