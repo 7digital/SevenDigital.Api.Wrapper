8c124069adcaad31302e3ca3dac96bb219c19f0d
AnthonySteele <Anthony@PlasticAvalon.org>
2014-06-08 17:45:44 +0100

    integration test on ResponseAs() Will check later if this works.

--- src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/StatusTests.cs
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/StatusTests.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Xml.Serialization;
+
 using NUnit.Framework;
 using SevenDigital.Api.Schema;
 
 namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests
 {
+	[XmlRoot("response")]
+	public class RawStatusResponse
+	{
+		public Status Status { get; set; }
+	}
+
 	[TestFixture]
 	public class StatusTests
 	{
@@ -14,6 +22,16 @@ namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests
 
 			Assert.That(status, Is.Not.Null);
 			Assert.That(status.ServerTime.Day, Is.EqualTo(DateTime.Now.Day));
+		}
+
+		[Test]
+		public async void TestStatusResponseAs()
+		{
+			var statusResponse = await Api<Status>.Create.ResponseAs<RawStatusResponse>();
+
+			Assert.That(statusResponse, Is.Not.Null);
+			Assert.That(statusResponse.Status, Is.Not.Null);
+			Assert.That(statusResponse.Status.ServerTime.Day, Is.EqualTo(DateTime.Now.Day));
 		}
 	}
 }
