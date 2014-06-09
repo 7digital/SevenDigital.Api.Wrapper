a131f73bb53fbf3f17dd82ec47eb17935da112ae
Anthony Steele <Anthony.Steele@7digital.com>
2014-06-09 11:43:51 +0100

    Moved responseAs test to different file as it's not the same thing as the StatusTests - it's using the status endpoint as a simple e.g. of a working endpoint, not testing that it is working

--- src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/StatusTests.cs
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/StatusTests.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using System.Xml.Serialization;
 
 using NUnit.Framework;
 using SevenDigital.Api.Schema;
 
 namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests
 {
-	[XmlRoot("response")]
-	public class RawStatusResponse
-	{
-		[XmlElement("serviceStatus")]
-		public Status ServiceStatus { get; set; }
-	}
-
 	[TestFixture]
 	public class StatusTests
 	{
@@ -23,16 +15,6 @@ namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests
 
 			Assert.That(status, Is.Not.Null);
 			Assert.That(status.ServerTime.Day, Is.EqualTo(DateTime.Now.Day));
-		}
-
-		[Test]
-		public async void TestStatusResponseAs()
-		{
-			var statusResponse = await Api<Status>.Create.ResponseAs<RawStatusResponse>();
-
-			Assert.That(statusResponse, Is.Not.Null);
-			Assert.That(statusResponse.ServiceStatus, Is.Not.Null);
-			Assert.That(statusResponse.ServiceStatus.ServerTime.Day, Is.EqualTo(DateTime.Now.Day));
 		}
 	}
 }
