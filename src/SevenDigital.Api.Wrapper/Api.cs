eeebd5113f0270cfadbaa44aa27963927e274d8c
Anthony Steele <Anthony.Steele@7digital.com>
2014-06-20 13:25:23 +0100

    Static api is working for simple case And you can plug in your own api factory Removed create with creds - using your own non-static api is the proper customization point for this This is demonstrated in ApiSetupCredentialPassingTests.cs

--- src/SevenDigital.Api.Wrapper/Api.cs
+++ src/SevenDigital.Api.Wrapper/Api.cs
@@ -12,17 +12,4 @@
 			return new FluentApi<T>();
 		}
 	}
-
-	public class Api<T> where T : class, new()
-	{
-		public static IFluentApi<T> Create
-		{
-			get { return new FluentApi<T>(); }
-		}
-
-		public static IFluentApi<T> CreateWithCreds(IOAuthCredentials oAuthCredentials, IApiUri apiUri)
-		{
-			return new FluentApi<T>(oAuthCredentials, apiUri);
-		}
-	}
 }
---
+++ src/SevenDigital.Api.Wrapper/StaticApi.cs
@@ -0,0 +1,22 @@
+﻿namespace SevenDigital.Api.Wrapper
+{
+	public static class ApiForward
+	{
+		public static IApi ApiFactory { get; set; }
+	}
+
+	public static class Api<T> where T : class, new()
+	{
+		public static IFluentApi<T> Create
+		{
+			get
+			{
+				if (ApiForward.ApiFactory == null)
+				{
+					ApiForward.ApiFactory = new ApiFactory();
+				}
+				return ApiForward.ApiFactory.Create<T>();
+			}
+		}
+	}
+}
