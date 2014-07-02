33d0afeb1b86838c765f29dba734cdf1983e4ec5
Anthony Steele <Anthony.Steele@7digital.com>
2014-07-02 09:13:21 +0100

    Fixed integration tests on endpoints for release, release tags and release tracks as release #155408 has been replaced with release #3070977 with similar but not identical data. This is a data issue that will affect all branches

--- src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/Tags/ReleaseTagsTests.cs
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/Tags/ReleaseTagsTests.cs
@@ -10,25 +10,30 @@ namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests.Tags
 		[Test]
 		public async void Can_hit_endpoint()
 		{
+			const string ExpectedTagId = "pop";
+
 			var request = Api<ReleaseTags>.Create
-				.WithParameter("releaseid", "155408");
-			var tags = await request.Please();
+				.WithParameter("releaseid", "3070977");
+			var releaseTags = await request.Please();
 
-			Assert.That(tags, Is.Not.Null);
-			Assert.That(tags.TagList.Count, Is.GreaterThan(0));
-			Assert.That(tags.TagList.FirstOrDefault().Id, Is.Not.Empty);
-			Assert.That(tags.TagList.Where(x => x.Id == "rock").FirstOrDefault().Text, Is.EqualTo("rock"));
+			Assert.That(releaseTags, Is.Not.Null);
+			Assert.That(releaseTags.TagList, Is.Not.Null);
+			Assert.That(releaseTags.TagList.Count, Is.GreaterThan(0));
 
+			Assert.That(releaseTags.TagList[0].Id, Is.Not.Empty);
+
+			var expectedTag = releaseTags.TagList.First(x => x.Id == ExpectedTagId);
+			Assert.That(expectedTag.Text, Is.EqualTo(ExpectedTagId));
 		}
 
 		[Test]
 		public async void Can_hit_endpoint_with_paging()
 		{
 			var request = Api<ReleaseTags>.Create
-				.WithParameter("releaseid", "155408")
+				.WithParameter("releaseid", "3070977")
 				.WithParameter("page", "2")
 				.WithParameter("pageSize", "1");
-			ReleaseTags releaseTags = await request.Please();
+			var releaseTags = await request.Please();
 
 			Assert.That(releaseTags, Is.Not.Null);
 			Assert.That(releaseTags.Page, Is.EqualTo(2));
