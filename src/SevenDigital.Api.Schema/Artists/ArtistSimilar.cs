039723e334b16fa67e55cf807134477fb3fbecf5
emma-ashley.liles <emma-ashley.liles@7digital.com>
2014-08-29 11:11:52 +0100

    Added a class for "artist/similar" endpoint.

---
+++ src/SevenDigital.Api.Schema/Artists/ArtistSimilar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using SevenDigital.Api.Schema.Attributes;
+using SevenDigital.Api.Schema.ParameterDefinitions.Get;
+
+namespace SevenDigital.Api.Schema.Artists
+{
+	[ApiEndpoint("artist/similar")]
+	[XmlRoot("artists")]
+	public class ArtistSimilar : HasPaging, HasArtistIdParameter
+	{
+		[XmlElement("artist")]
+		public List<Artist> Artists { get; set; }
+	}
+}
---
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/Artists/ArtistSimilarTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using SevenDigital.Api.Schema.Artists;
+
+namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests.Artists
+{
+	public class ArtistSimilarTests
+	{
+		[Test]
+		public async void Can_hit_endpoint()
+		{
+			var request = Api<ArtistSimilar>
+				.Create
+				.WithArtistId(1)
+				.WithParameter("country", "GB");
+			var artistSimilar = await request.Please();
+
+			Assert.That(artistSimilar, Is.Not.Null);
+		}
+
+		[Test]
+		public async void Can_get_multiple_results()
+		{
+			var request = Api<ArtistSimilar>.Create
+				.WithArtistId(1)
+				.WithParameter("page", "1")
+				.WithParameter("pageSize", "20");
+			var artistSimilar = await request.Please();
+
+			Assert.That(artistSimilar.Artists.Count, Is.GreaterThan(1));
+		}
+
+		[Test]
+		public async void Can_do_paging()
+		{
+			var request = Api<ArtistSimilar>
+				.Create
+				.WithArtistId(1)
+				.WithParameter("page", "2")
+				.WithParameter("pageSize", "2");
+			var artistSimilar = await request.Please();
+
+			Assert.That(artistSimilar.Page, Is.EqualTo(2));
+			Assert.That(artistSimilar.Artists.Count, Is.EqualTo(2));
+
+		}
+	}
+
+
+	
+}
