e179eb7f6bf0aefbb641f327e4291bb8a17da8ed
Anthony Steele <anthony.steele@7digital.com>
2015-04-28 17:40:08 +0100

    Integration tests on InMemoryResponseCache Three methods: Please(), Response() and ResponseAs<T>() Should all leverage the cache

---
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/Http/InMemoryResponseCacheTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Net;
+using System.Xml.Serialization;
+using NUnit.Framework;
+using SevenDigital.Api.Schema.Artists;
+using SevenDigital.Api.Wrapper.Responses;
+
+namespace SevenDigital.Api.Wrapper.Integration.Tests.Http
+{
+	[XmlRoot("response")]
+	public class RawArtistChartResponse
+	{
+		[XmlElement("chart")]
+		public ArtistChart Chart { get; set; }
+	}
+	
+	
+	[TestFixture]
+	public class InMemoryResponseCacheTests
+	{
+		private static readonly IResponseCache _cache = new InMemoryResponseCache();
+
+		[Test]
+		public async void DataIsCachedWhenCacheIsUsed()
+		{
+			var response1 = await Api<ArtistChart>
+				.Create
+				.UsingCache(_cache)
+				.Please();
+
+			var response2 = await Api<ArtistChart>
+				.Create
+				.UsingCache(_cache)
+				.Please();
+
+			Assert.That(response2, Is.EqualTo(response1));
+		}
+
+		[Test]
+		public async void DataIsNotCachedWhenCacheIsNotUsed()
+		{
+			var response1 = await Api<ArtistChart>
+				.Create
+				.Please();
+
+			var response2 = await Api<ArtistChart>
+				.Create
+				.Please();
+
+			Assert.That(response2, Is.Not.EqualTo(response1));
+		}
+
+		[Test]
+		public async void ResponseIsCachedWhenCacheIsUsed()
+		{
+			var response1 =  await Api<ArtistChart>
+				.Create
+				.UsingCache(_cache)
+				.Response();
+
+			AssertIsCacheableResponse(response1);
+
+			var response2 = await Api<ArtistChart>
+				.Create
+				.UsingCache(_cache)
+				.Response();
+
+			AssertIsCacheableResponse(response2);
+			Assert.That(response2, Is.EqualTo(response1));
+		}
+
+		[Test]
+		public async void ResponseIsNotCachedWhenCacheIsNotUsed()
+		{
+			var response1 = await Api<ArtistChart>
+				.Create
+				.Response();
+
+			AssertIsCacheableResponse(response1);
+
+			var response2 = await Api<ArtistChart>
+				.Create
+				.Response();
+
+			AssertIsCacheableResponse(response2);
+			Assert.That(response2, Is.Not.EqualTo(response1));
+		}
+
+		[Test]
+		public async void ResponseAsIsCachedWhenCacheIsUsed()
+		{
+			var response1 = await Api<ArtistChart>
+				.Create
+				.UsingCache(_cache)
+				.ResponseAs<RawArtistChartResponse>();
+
+			var response2 = await Api<ArtistChart>
+				.Create
+				.UsingCache(_cache)
+				.ResponseAs<RawArtistChartResponse>();
+
+			Assert.That(response2, Is.EqualTo(response1));
+		}
+
+		[Test]
+		public async void ResponseAsIsNotCachedWhenCacheIsNotUsed()
+		{
+			var response1 = await Api<ArtistChart>
+				.Create
+				.ResponseAs<RawArtistChartResponse>();
+
+			var response2 = await Api<ArtistChart>
+				.Create
+				.ResponseAs<RawArtistChartResponse>();
+
+			Assert.That(response2, Is.Not.EqualTo(response1));
+		}
+
+		private static void AssertIsCacheableResponse(Response response)
+		{
+			Assert.That(response, Is.Not.Null);
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(response.Headers.ContainsKey("cache-control"), Is.True);
+			Assert.That(response.Headers["cache-control"], Is.StringContaining("max-age="));
+		}
+	}
+}
