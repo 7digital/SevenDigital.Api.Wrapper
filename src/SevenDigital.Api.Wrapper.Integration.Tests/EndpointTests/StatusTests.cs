b45394c545b23a9dbd5804d64aa346eceb8e53ee
Greg Sochanik <greg.sochanik@7digital.com>
2015-09-18 13:04:40 +0100

    Added another test to ensure 2 different custom traceIds result in 2 different response traceIds

--- src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/StatusTests.cs
+++ src/SevenDigital.Api.Wrapper.Integration.Tests/EndpointTests/StatusTests.cs
@@ -53,5 +53,19 @@ namespace SevenDigital.Api.Wrapper.Integration.Tests.EndpointTests
 
 			Assert.That(status.OriginalRequest.TraceId, Is.EqualTo(customTraceId));
 		}
+
+		[Test]
+		public async void Can_specify_2_different_trace_ids_on_different_requests()
+		{
+			var customTraceId1 = Guid.NewGuid().ToString();
+			var customTraceId2 = Guid.NewGuid().ToString();
+
+			var fluentApi = Api<Status>.Create;
+			var status = await fluentApi.WithTraceId(customTraceId1).Response();
+			var status2 = await fluentApi.WithTraceId(customTraceId2).Response();
+
+			Assert.That(status.OriginalRequest.TraceId, Is.EqualTo(customTraceId1));
+			Assert.That(status2.OriginalRequest.TraceId, Is.EqualTo(customTraceId2));
+		}
 	}
 }
